c537c0c937ab5367aedbd509d7e9778ba3e34a63
FieryBlade-313 <mohdfauz.s18@iiits.in>
2020-10-27 21:54:06 +0530

    Intermediate step of cleaning the repo of problematic files

--- IndieFaction/colledition/models.py
+++ IndieFaction/colledition/models.py
@@ -3,6 +3,21 @@ import uuid
 import accounts.models as acc_models
 # Create your models here.
 
+class Images(models.Model):
+    img_id = models.UUIDField(default=uuid.uuid4,verbose_name='Image ID',primary_key=True,editable=False)
+    url_thumbnail = models.URLField(verbose_name='Thumbnail URL')
+    url_high_res = models.URLField(verbose_name='High Resolution URL')
+
+    def __str__(self):
+        return self.url_thumbnail
+
+
+
+class CollectorsEditionGenre(models.Model):
+    genre_name = models.CharField(max_length=50,verbose_name='Genre Name')
+
+    def __str__(self):
+        return self.genre_name
 
 class CollectorEdition(models.Model):
     cid = models.UUIDField(default=uuid.uuid4,verbose_name='CID',primary_key=True,editable=False)
@@ -12,27 +27,9 @@ class CollectorEdition(models.Model):
     date_of_creation = models.DateTimeField(verbose_name='date created', auto_now_add=True)
     uid = models.ForeignKey(acc_models.IndieUser,verbose_name='User ID',on_delete=models.CASCADE)
     game_name = models.CharField(max_length=50,verbose_name='Game Name')
+    images = models.ManyToManyField(Images,verbose_name="Images")
+    genre = models.ManyToManyField(CollectorsEditionGenre,verbose_name="Genre")
 
     def __str__(self):
         return 'Game '+self.name+' owned by '+str(self.uid)
-
-
-class Images(models.Model):
-    img_id = models.UUIDField(default=uuid.uuid4,verbose_name='Image ID',primary_key=True,editable=False)
-    cid = models.ManyToManyField(CollectorEdition,verbose_name="Collector's edition ID")
-    url_thumbnail = models.URLField(verbose_name='Thumbnail URL')
-    url_high_res = models.URLField(verbose_name='High Resolution URL')
-
-    def __str__(self):
-        used_names = [x.name for x in self.cid.all() ]
-        return 'Image used by games: '+','.join(used_names)
-
-
-
-class CollectorsEditionGenre(models.Model):
-    cid = models.ManyToManyField(CollectorEdition,verbose_name="Collector's edition ID")
-    genre_name = models.CharField(max_length=50,verbose_name='Genre Name')
-
-    def __str__(self):
-        return self.genre_name
 
