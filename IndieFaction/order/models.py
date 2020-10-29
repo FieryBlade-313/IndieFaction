0c4f7f1c9502cdfa201bd9a079c5df9269d0ab8c
FieryBlade-313 <mohdfauz.s18@iiits.in>
2020-10-29 12:13:28 +0530

    Exposed Order API

--- IndieFaction/order/models.py
+++ IndieFaction/order/models.py
@@ -4,22 +4,31 @@ from colledition.models import CollectorEdition
 from accounts.models import UserName, Address
 from django.core.validators import RegexValidator
 
-class Order(UserName,Address):
-    order_id = models.UUIDField(default=uuid.uuid4,verbose_name='order_id',primary_key=True,editable=False)
-    contact_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    contact_no = models.CharField(validators=[contact_regex], max_length=17, blank=False)
-    token = models.CharField(max_length=50,verbose_name='Token',default='#T',unique=True)
-    status = models.CharField(max_length=50,verbose_name='Status',default='NA')
-    cid = models.ForeignKey(CollectorEdition, verbose_name=("cid"), on_delete=models.CASCADE)
+
+class Order(UserName, Address):
+    order_id = models.UUIDField(
+        default=uuid.uuid4, verbose_name='order_id', primary_key=True, editable=False)
+    contact_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    contact_no = models.CharField(
+        validators=[contact_regex], max_length=17, blank=False)
+    token = models.CharField(
+        max_length=50, verbose_name='Token', default='#T', unique=True)
+    status = models.CharField(
+        max_length=50, verbose_name='Status', default='NA', editable=False)
+    cid = models.ForeignKey(CollectorEdition, verbose_name=(
+        "cid"), on_delete=models.CASCADE)
 
     def __str__(self):
-        self.cid.name+" "+self.status
+        return self.cid.name+" "+self.status
 
     class Meta:
         abstract = True
 
+
 class PrintOrder(Order):
     pass
+
 
 class CompletedOrder(Order):
     pass
--- IndieFaction/order/views.py
+++ IndieFaction/order/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from .serializers import *
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from .models import PrintOrder, CompletedOrder
+
+
+@api_view(['GET'])
+def printOrder(request):
+
+    if request.method == 'GET':
+        if len(request.GET) == 1:
+            try:
+                token = request.GET['token']
+            except:
+                return JsonResponse({'code': 400, 'message': 'Bad Input Parameter'})
+            try:
+                order = PrintOrder.objects.get(token=token)
+            except:
+                return JsonResponse({'code': 404, 'message': 'Invalid Token'})
+            serializer = PrintOrderSerializer(
+                order, context={'request': request})
+            return Response(serializer.data)
+        else:
+            return JsonResponse({'code': 400, 'message': 'Bad Input Parameter'})
